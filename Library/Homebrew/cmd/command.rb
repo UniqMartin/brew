5cd6af345d8f154a7cd43b25c0a2796b9475f307
Martin Afanasjew <martin@afanasjew.de>
2016-05-25 07:16:10 +0200

    command: use command abstraction

--- Library/Homebrew/cmd/command.rb
+++ Library/Homebrew/cmd/command.rb
@@ -3,29 +3,15 @@
 
 module Homebrew
   def command
-    abort "This command requires a command argument" if ARGV.empty?
-    cmd = ARGV.first
-    cmd = HOMEBREW_INTERNAL_COMMAND_ALIASES.fetch(cmd, cmd)
+    if ARGV.empty?
+      raise UsageError, "This command requires a command argument"
+    end
 
-    if (path = internal_command_path cmd)
-      puts path
-    elsif (path = which("brew-#{cmd}") || which("brew-#{cmd}.rb"))
-      puts path
-    else
+    cmd = Command[ARGV.first]
+    if cmd.unknown?
       odie "Unknown command: #{cmd}"
+    else
+      puts cmd.path
     end
-  end
-
-  private
-
-  def internal_command_path(cmd)
-    extensions = %w[rb sh]
-    paths = extensions.map { |ext| HOMEBREW_LIBRARY_PATH/"cmd/#{cmd}.#{ext}" }
-
-    if ARGV.homebrew_developer?
-      paths += extensions.map { |ext| HOMEBREW_LIBRARY_PATH/"dev-cmd/#{cmd}.#{ext}" }
-    end
-
-    paths.find { |p| p.file? }
   end
 end
