15bcdf0f8793f926c0c771b4c492853027799012
Martin Afanasjew <martin@afanasjew.de>
2016-07-10 04:05:23 +0200

    git_repository: resolve rewrites of remote URL

--- Library/Homebrew/extend/git_repository.rb
+++ Library/Homebrew/extend/git_repository.rb
@@ -6,10 +6,19 @@ module GitRepositoryExtension
     join(".git").exist?
   end
 
-  def git_origin
+  def git_origin(remote = "origin")
     return unless git? && Utils.git_available?
     cd do
-      Utils.popen_read("git", "config", "--get", "remote.origin.url").chuzzle
+      # Much nicer than the fallback below, but requires Git 2.7.0 or newer.
+      url = Utils.popen_read("git", "remote", "get-url", remote).chuzzle
+      return url if $?.success?
+
+      # Works with all Git versions, but requires grepping & additional checks.
+      info = Utils.popen_read("git", "remote", "show", "-n", remote)
+      url = info[/^\s+Fetch URL: (.+)$/, 1]
+      return url unless url == remote
+
+      # Return nil if all attempts failed (remote doesn't exist or has no URL).
     end
   end
 
